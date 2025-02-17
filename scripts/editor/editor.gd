d89b7e7ddfd4c9292e82214e39210fbf6c34a72d
Barkerprooks <jonparkerbrooks@gmail.com>
2025-02-17 12:34:09 -0600

    progress

--- scripts/editor/editor.gd
+++ scripts/editor/editor.gd
@@ -1,23 +1,25 @@
 extends Control
 
 
-const SCENE_SAVE_PATH: String = "SceneEditor/Scenes"
-const ASSET_SAVE_PATH: String = "SceneEditor/Assets"
+const SCENE_PATH: String = "SceneEditor/Scenes"
+const ACTOR_PATH: String = "SceneEditor/Assets/Actors"
+const BACKGROUND_PATH: String = "SceneEditor/Assets/Backgrounds"
 
 static var scene: Dictionary = { "title": "untitled", "pages": [] }
 
 static var open_file_dialog: FileDialog
 static var save_file_dialog: FileDialog
 
-static var title: Label
+static var background: ImageTexture
+static var title: LineEdit
 
 
-static func __get_scene_save_path() -> String:
-	var scene_save_path: String = OS.get_system_dir(OS.SYSTEM_DIR_DOCUMENTS).path_join(SCENE_SAVE_PATH)
-	if not DirAccess.dir_exists_absolute(scene_save_path):
-		print_debug("created scenes folder at %s" % scene_save_path)
-		DirAccess.make_dir_recursive_absolute(scene_save_path)
-	return scene_save_path
+static func __get_data_path(path: String) -> String:
+	var data_path: String = OS.get_system_dir(OS.SYSTEM_DIR_DOCUMENTS).path_join(path)
+	if not DirAccess.dir_exists_absolute(data_path):
+		print_debug("created scenes folder at %s" % data_path)
+		DirAccess.make_dir_recursive_absolute(data_path)
+	return data_path
 
 
 static func __load_scene(path: String) -> void:
@@ -35,41 +37,48 @@ static func __dump_scene(path: String) -> void:
 
 static func new_scene() -> void:
 	scene = { "title": "untitled", "pages": [] }
+	title.text = "untitled"
+	# background.create_placeholder()
 
 
 static func save_scene() -> void:
-	var scene_save_path: String = __get_scene_save_path()
-	__dump_scene(scene_save_path.path_join(".".join([scene["title"], "json"])))
+	var path: String = __get_data_path(SCENE_PATH)
+	var file: String = ".".join([scene["title"], "json"])
+	__dump_scene(path.path_join(file))
 
 
 static func save_scene_as() -> void:
-	save_file_dialog.current_dir = __get_scene_save_path()
+	open_file_dialog.filters = ["*.json"]
+	save_file_dialog.current_dir = __get_data_path(SCENE_PATH)
 	save_file_dialog.connect("file_selected", __dump_scene)
 	save_file_dialog.popup()
 
 
 static func open_scene() -> void:
-	open_file_dialog.current_dir = __get_scene_save_path()
+	open_file_dialog.filters = ["*.json"]
+	open_file_dialog.current_dir = __get_data_path(SCENE_PATH)
 	open_file_dialog.connect("file_selected", __load_scene)
 	open_file_dialog.popup()
 
 
-static func __load_asset(path: String) -> void:
-	pass
+static func __load_background_asset(path: String) -> void:
+	var image: Image = Image.new()
+	image.load(path)
+	background.set_image(image)
 
 
 static func import_background() -> void:
+	open_file_dialog.filters = ["*.png,*.jpg,*.jpeg,*.gif,*.svg"]
 	open_file_dialog.current_dir = OS.get_system_dir(OS.SYSTEM_DIR_DOCUMENTS)
-	open_file_dialog.connect("file_selected", __load_asset)
+	open_file_dialog.connect("file_selected", __load_background_asset)
 	open_file_dialog.popup()
-
-
-static func import_actor() -> void:
-	pass
 
 
 func _ready() -> void:
 	open_file_dialog = $OpenFileDialog
 	save_file_dialog = $SaveFileDialog
-	title = $HSplitContainer/MarginContainer/Title
+
+	title = $HSplit/Header/HSplit/TitleInput
 	title.text = scene["title"]
+
+	background = $HSplit/Body/HSplit/VSplit/Background.texture
