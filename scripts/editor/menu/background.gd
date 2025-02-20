5c15e946fb3cbd4e3060b27e1ecd5c2126d3b1eb
Barkerprooks <jonparkerbrooks@gmail.com>
2025-02-19 20:12:40 -0600

    editing scripts

--- scripts/editor/menu/background.gd
+++ scripts/editor/menu/background.gd
@@ -5,13 +5,20 @@ extends PopupMenu
 
 
 func __index_pressed(index: int) -> void:
-	var item_text = get_item_text(index)
-	Editor.debug("loading background %s" % item_text)
-	Editor.load_background(item_text)
+	var string = get_item_text(index)
+	
+	if string == "import background" and index == 0:
+		Editor.debug("importing background")
+		Editor.import_background()
+	else:
+		Editor.debug("loading background %s" % string)
+		Editor.load_background(string)
 
 
 func set_background_list() -> void:
 	clear()
+	add_item("import background")
+	add_separator()
 	for filename in Editor.list_backgrounds():
 		add_item(filename)
 
